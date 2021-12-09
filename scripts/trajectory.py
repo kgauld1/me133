01ef5ff090ed99412851942e7312ef39b5c85bd6
maishakhanum <mkhanum@caltech.edu>
2021-12-08 17:07:18 -0800

    trajectory class
    returns position when given velocity, launch angle, and direction

--- scripts/trajectory.py
+++ scripts/trajectory.py
@@ -1,72 +1,78 @@
 #!/usr/bin/env python3
 #
-#   splines.py
-#
-#   TO IMPORT, ADD TO YOUR CODE:
-#   from splines import CubicSpline, Goto, Hold, Stay, QuinticSpline, Goto5
-#
-#
-#   This library provides the cubic and quintic spline trajectory
-#   segment classes.  I seperated from the main code, just so I don't
-#   have to copy/paste it again and again....
-#
-#   The classes are:
-#
-#      seg = CubicSpline(p0, v0, pf, vf, T, space='Joint')
-#      seg = Goto(       p0,     pf,     T, space='Joint')
-#      seg = Hold(       p,              T, space='Joint')
-#      seg = Stay(       p,                 space='Joint')
-#
-#      seg = QuinticSpline(p0, v0, a0, pf, vf, af, T, space='Joint')
-#      seg = Goto5(        p0,         pf,         T, space='Joint')
-#
-#   Each spline object then provides a
-#
-#      (pos, vel) = seg.evaluate(t)
-#      T          = seg.duration()
-#      space      = seg.space()
-#
-#   That is, each segment can compute the position and velocity for
-#   the specified time.  Note it can also report which "space" it
-#   wants, but inthe end it is the user's responsibility to
-#   interpret/use the pos/vel information accordingly.
-#
-#   The segments also assume a t=0 start time.  That is, when calling
-#   evaluate(), please first subtract the start time of the segment!
-#
-#   The p0,pf,v0,vf,a0,af may be NumPy arrays.
-#
+
+
 import math
+import numpy as np
+import rospy
 
 
 class Trajectory:
-    def __init__(self, params_for_motion):
+    def __init__(self, velocity, angle, xyangle):
+        # velocity: number
+        # angle: launch angle, 
+        # xyangle: direction launched (xy plane)
+        
+        self.pos = np.array([0.0, 2.0, 0.0])
+        self.vel = velocity
+        self.angle = angle
+        self.xyangle = xyangle
+        self.grav = 2.0
+    
     	# todo -- define path in terms of t (or just set params)
         return
-    def getPath(t):
+        
+    def getPath(self, t):
+        vx = self.vel*np.cos(self.angle)*np.cos(self.xyangle)
+        vy = self.vel*np.cos(self.angle)*np.sin(self.xyangle)
+        vz = self.vel*np.sin(self.angle) - self.grav * t
+#        vz = self.vel*np.sin(self.angle)
+        
+        
+        
+        
         # todo -- get p of the ball for a time t 
-        return 
+        print (np.array([self.pos[0]+vx*t, self.pos[1]+vy*t, self.pos[2]+vz*t - 0.5*self.grav*t**2]))
+        
+        return np.array([self.pos[0]+vx*t, 
+                         self.pos[1]+vy*t, 
+                         self.pos[2]+vz*t - 0.5*self.grav*t**2])
 
 
 
+# TO TEST TRAJECTORIES
+#
+
+#if __name__ == "__main__":
+#    # Prepare/initialize this node.
+#    rospy.init_node('traj')
+
+#    # Instantiate the trajectory generator object, encapsulating all
+#    # the computation and local variables.
+#    traj = Trajectory(5,45,0)
+
+#    # Prepare a servo loop at 100Hz.
+#    rate  = 100;
+#    servo = rospy.Rate(rate)
+#    dt    = servo.sleep_dur.to_sec()
+#    rospy.loginfo("Running the servo loop with dt of %f seconds (%fHz)" %
+#                  (dt, rate))
 
 
-class Linear:
-    def __init__(self, p0, pf, T, space='Joint'):
-        self.p0 = p0
-        self.T = T
-        self.vel = (pf - p0)/T
-        self.usespace = space
-    
-    # Return the segment's space
-    def space(self):
-        return self.usespace
+#    # Run the servo loop until shutdown (killed or ctrl-C'ed).
+#    t = 0
+#    while not rospy.is_shutdown():
 
-    # Report the segment's duration (time length).
-    def duration(self):
-        return self.T
-    
-    def evaluate(self, t):
-        p = self.p0 + self.vel * t
-        return (p, self.vel)
+#        # Update the controller.
+#            
+#        print(traj.getPath(t))
+
+
+#        # Wait for the next turn.  The timing is determined by the
+#        # above definition of servo.
+#        servo.sleep()
+
+#        # Update the time.
+#        t += dt
+
 
